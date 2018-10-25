25df65643ceb8e44eae7630a740d7c043e0938a6
DanGalkin <dangalkin@gmail.com>
2018-10-25 23:38:27 +0300

    Added the type for except

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -5,7 +5,7 @@ def get_roots(a, b, c):
     try:
         root1 = (-b - sqrt(discriminant)) / (2 * a)
         root2 = (-b + sqrt(discriminant)) / (2 * a)
-    except:
+    except ValueError:
     	return None, None
     if discriminant == 0:
         return root1, None
